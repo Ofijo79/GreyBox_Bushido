bb9728055d838c33d8660bb31425511d5506b95b
Ofijo79 <oriolfijo2004@gmail.com>
2024-02-23 13:50:37 +0100

    Juego

--- Assets/Scripts/EnemyLejos.cs
+++ Assets/Scripts/EnemyLejos.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyLejos : MonoBehaviour
 {
+    [SerializeField] Transform gunPosition;
+
+    [SerializeField] int bulletType = 0;
+
     public GameObject flechaPrefab;
-    public Transform firePoint;
-    public float flechaSpeed = 20f;
+    public float flechaForce = 20f;
     public float fireRate = 1f;
     private float nextFireTime = 0f;
     // Start is called before the first frame update
@@ -18,7 +21,7 @@ public class EnemyLejos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextFireTime)
+        if (Time.time >= nextFireTime)
         {
             Shoot();
             nextFireTime = Time.time + 1f / fireRate;
@@ -27,8 +30,6 @@ public class EnemyLejos : MonoBehaviour
 
     void Shoot()
     {
-        GameObject flecha = Instantiate(flechaPrefab, firePoint.position, firePoint.rotation);
-        //RigidBody rbody = flecha.GetComponent<Rigidbody>();
-        //rbody.AddForce(firePoint.forward * flechaSpeed, ForceMode.Impulse);
+        GameObject projectile = Instantiate(flechaPrefab, gunPosition.position, gunPosition.rotation);
     }
 }
