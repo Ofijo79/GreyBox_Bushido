ce2f4188a8765e920d341a397cbca909b017d8a5
Ofijo79 <oriolfijo2004@gmail.com>
2024-02-23 13:09:02 +0100

    PorSiacaso

--- Assets/Scripts/EnemyLejos.cs
+++ Assets/Scripts/EnemyLejos.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyLejos : MonoBehaviour
 {
+    public GameObject flechaPrefab;
+    public Transform firePoint;
+    public float flechaSpeed = 20f;
+    public float fireRate = 1f;
+    private float nextFireTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class EnemyLejos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject flecha = Instantiate(flechaPrefab, firePoint.position, firePoint.rotation);
+        //RigidBody rbody = flecha.GetComponent<Rigidbody>();
+        //rbody.AddForce(firePoint.forward * flechaSpeed, ForceMode.Impulse);
     }
 }
